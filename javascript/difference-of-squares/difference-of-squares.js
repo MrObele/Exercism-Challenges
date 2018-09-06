e0835125a59aaa5656429e2f993a6429b2a8067e
mrobele <obeledavid@yahoo.com>
2018-09-06 09:58:46 +0100

    finished with the difference

--- javascript/difference-of-squares/difference-of-squares.js
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -4,40 +4,36 @@
     }
      
 
-    
-  }
-
-  Squares.prototype.squareOfSum = function() {
-    let squareOfSumArray = [];
+    get squareOfSum () {
+    let squareOfSumArray = 0;
     for (let i = 1; i <= this.num; i++) {
-    squareOfSumArray.push(i);
+    squareOfSumArray+= i;
     }
-    return Math.pow(squareOfSumArray.reduce((a, b) => a + b, 0), 2);
+    return Math.pow(squareOfSumArray, 2);
   }
 
-  Squares.prototype.sumOfSquares = function() {
-    let sumOfSquaresArray = [];
+
+
+ get sumOfSquares() {
+    let sumOfSquaresArray = 0;
     for (let i = 1; i <= this.num; i++) {
-      sumOfSquaresArray.push(i * i);
+      sumOfSquaresArray += Math.pow(i,2);
     }
 
-    return sumOfSquaresArray.reduce((a, b) => a + b, 0);
+    return sumOfSquaresArray;
   }
 
-  Squares.prototype.difference = function() {
-     return this.squareOfSum() - this.sumOfSquares();
+
+ get ldifference() {
+     return this.squareOfSum - this.sumOfSquares;
+  }
+  
+
   }
   
 
 
 export default Squares;
 
-let squares5 = new Squares(100);
-
-console.log(squares5.sumOfSquares());
-
-console.log(squares5.squareOfSum());
-
-console.log(squares5.difference());
 
 
