504b1222095cba9581815d8a9252d189900acb78
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2018-09-12 09:50:40 +0100

    using array method to solve the isogram challenge

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,31 +1,39 @@
 class Isogram {
-	constructor(userInput){
-		this.word = userInput.toLowerCase().replace(/[^\w]/g, '');
-	}
+  constructor(userInput) {
+    this.word = userInput.toLowerCase().replace(/[^\w]/g, "");
+  }
 
-	isIsogram(){
- 
-	  let checker = 0;
-      let counter = 0;
-      if(this.word === ""){
-      	return true;
-      }
-	  for(let i = 0; i<this.word.length;i++){
-	  	counter = 0;
-	  	for(let j = 0; j<this.word.length;j++){
-	  		if(this.word[i] === this.word[j]){
-	  			counter+= 1;
-	  		}
-	  	}
-	  	 if(counter >1){
-	  	 	checker+= 1;
-	  	 }
-	  }
-	  if(checker === 0 ){
-	  	return true;
-	  }
-	  	return false;
-	}
+  isIsogram() {
+    let sentence = this.word.split("").sort();
+    for (let i = 0; i < sentence.length; i++) {
+      if (sentence[i] == sentence[i + 1]) return false;
+    }
+    return true;
+  }
 }
 
 export default Isogram;
+
+// isIsogram(){
+
+// 	let checker = 0;
+// 		let counter = 0;
+// 		if(this.word === ""){
+// 			return true;
+// 		}
+// 	for(let i = 0; i<this.word.length;i++){
+// 		counter = 0;
+// 		for(let j = 0; j<this.word.length;j++){
+// 			if(this.word[i] === this.word[j]){
+// 				counter+= 1;
+// 			}
+// 		}
+// 		 if(counter >1){
+// 			 checker+= 1;
+// 		 }
+// 	}
+// 	if(checker === 0 ){
+// 		return true;
+// 	}
+// 		return false;
+// }
