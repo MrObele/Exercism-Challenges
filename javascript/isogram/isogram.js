e4a3021b1a24564b12e7c1d8b9d175fbbd42cd4f
mrobele <obeledavid@yahoo.com>
2018-09-12 09:30:55 +0100

    finished with isogram

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -4,22 +4,27 @@ class Isogram {
 	}
 
 	isIsogram(){
-	  let counter = 0;
-
-	  for(let i=0; i<this.word.length;i++){
+ 
+	  let checker = 0;
+      let counter = 0;
+      if(this.word === ""){
+      	return true;
+      }
+	  for(let i = 0; i<this.word.length;i++){
+	  	counter = 0;
 	  	for(let j = 0; j<this.word.length;j++){
-	  		if(word[i] == word[j]){
+	  		if(this.word[i] === this.word[j]){
 	  			counter+= 1;
 	  		}
 	  	}
 	  	 if(counter >1){
-	  	 	return false;
-	  	 }else{
-	  	 	counter= 0;
+	  	 	checker+= 1;
 	  	 }
 	  }
-	  return true
-	 	
+	  if(checker === 0 ){
+	  	return true;
+	  }
+	  	return false;
 	}
 }
 
